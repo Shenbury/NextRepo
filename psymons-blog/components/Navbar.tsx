d44809dafb9639ab5557b4101c5be3eb8aae712f
Shenbury <simonhenbury@live.co.uk>
2022-10-01 12:03:01 +0100

    added toast

---
+++ psymons-blog/components/Navbar.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import Loader from "./Loader";
+
+export default function Navbar() {
+  const user = true;
+  const username = true;
+
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/">
+            <button className="btn-logo">Feed</button>
+          </Link>
+        </li>
+
+        {/* User is signed-in and has username*/}
+        {username && (
+          <>
+            <li>
+              <Link href="/Admin">
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <img src={user?.photoUrl} />
+              </Link>
+            </li>
+          </>
+        )}
+
+        {/* User is not signed in OR has not created username*/}
+        {!username && (
+          <>
+            <li>
+              <Link href={"/enter"}>
+                <button className="btn-blue">Log in</button>
+              </Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
+}
--- psymons-blog/pages/index.tsx
+++ psymons-blog/pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from "next";
 import Loader from "../components/Loader";
 import styles from "../styles/Home.module.css";
+import toast from "react-hot-toast";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Loader show={true} />
+      <button onClick={() => toast.success("Hello Toast!")}> Toast Me </button>
     </div>
   );
 };
